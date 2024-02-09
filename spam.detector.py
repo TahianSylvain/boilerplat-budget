8e7acb5f4c46a6bfa4ae43fbdf434ad90f71264f
Sylvain <124794207+TahianSylvain@users.noreply.github.com>
2024-02-09 23:34:34 +0300

    Create spam.detector.py

---
+++ spam.detector.py
@@ -0,0 +1,15 @@
+# import libraries
+try:
+  # %tensorflow_version only exists in Colab.
+  !pip install tf-nightly
+except Exception:
+  pass
+import tensorflow as tf
+import pandas as pd
+from tensorflow import keras
+!pip install tensorflow-datasets
+import tensorflow_datasets as tfds
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
